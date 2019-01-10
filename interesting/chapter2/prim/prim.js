c3c49cb5480a8f07d9ad5cac73c4831cab598085
linquanchang_dxm <linquanchang_dxm@duxiaoman.com>
2019-01-10 11:23:14 +0800

    feat: 避圈法求解最小生成树

--- interesting/chapter2/prim/prim.js
+++ interesting/chapter2/prim/prim.js
@@ -1,7 +1,5 @@
 // 最小生成树
-
-const LinkList = require('./LinkList');
-const PriorityQueue = require('./PriorityQueue');
+// 避圈法：每次选择两个集合中的相连的最小边
 
 const data = [
     {u: 1, v: 2, w: 23},
@@ -20,25 +18,59 @@ const data = [
 const n = 7;
 
 function prim(netNodes, st) {
-    
+    const n = netNodes.length;
+    const closest = new Array(n).fill(st);
+    const lowcost = new Array(n);
+    const flag = new Array(n).fill(false);
+    netNodes[st].forEach((item, index) => {
+        lowcost[index] = item;
+    });
+    closest[st] = -1;
+    lowcost[st] = 0;
+    flag[st] = true;
+    for (let i = 1; i < n; ++i) {
+        let min = Infinity;
+        let minKey = -1;
+        for (let j = 0; j < n; ++j) {
+            if (!flag[j] && lowcost[j] < min) {
+                min = lowcost[j];
+                minKey = j;
+            }
+        }
+        if (minKey !== -1) {
+            flag[minKey] = true;
+            netNodes[minKey].forEach((item, index) => {
+                if (!flag[index] && lowcost[index] > item) {
+                    lowcost[index] = item;
+                    closest[index] = minKey;
+                }
+            });
+        } else {
+            console.log('无法构成最小生成树！');
+        }
+    }
+    console.log(closest);
+    console.log(lowcost);
+    console.log(lowcost.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, 0))
+
 }
 
-function main(data, n) {
-    const netNodes = new Array(n).fill(new Array(n).fill(Infinity));
+function main(data, n, st) {
+    const netNodes = new Array(n);
+    for (let i = 0; i < n; ++i) {
+        netNodes[i] = new Array(n).fill(Infinity);
+    }
     data.forEach(item => {
         let {u, v, w} = item;
         --u;
         --v;
         netNodes[u][v] = netNodes[v][u] = w;
     });
-    console.log(netNodes[5].head);
+    prim(netNodes, st)
 }
 
-main(data, n);
-
-
-
-
-
+main(data, n, 0);
 
 
