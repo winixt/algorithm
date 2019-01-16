b7e332c59b28e87b3588103d6fb9867bf895dffc
linquanchang_dxm <linquanchang_dxm@duxiaoman.com>
2019-01-16 21:52:05 +0800

    feat: 最长公共子序列算法10%

---
+++ interesting/chapter4/longestSubsequence.js
@@ -0,0 +1,18 @@
+// 最长子序列问题
+
+function longestSubsequence() {
+
+}
+
+const s1 = 'ABCADAB';
+const s2 = 'BACDBA';
+
+function main(s1, s2) {
+    const cache = [];
+    for (let i = 0; i < s1.length; ++i) {
+        
+    }
+}
+
+main(s1, s2);
+
