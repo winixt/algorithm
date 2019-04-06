f066233d9729b3c135cffbedea402bc2ce1fc5db
linquanchang_dxm <linquanchang_dxm@duxiaoman.com>
2019-04-06 23:04:03 +0800

    fix: 诡异 setTimeout 和 channel

--- __test__/test.js
+++ __test__/test.js
@@ -1,9 +1,37 @@
-(function __prof() {
-  console.log('hello 1')
-})();
+/**
+ * @param {number[]} price
+ * @param {number[][]} special
+ * @param {number[]} needs
+ * @return {number}
+ */
+var shoppingOffers = function(price, special, needs) {
+    const selects = [];
+    for (let item of special) {
+        let newNeeds = [];
+        const len = item.length - 1;
+        for (let i = 0; i < len; ++i) {
+            if (item[i] > needs[i]) {
+                newNeeds = null;
+                break;
+            } else {
+                newNeeds[i] = needs[i] - item[i];
+            }
+        }
+        if (newNeeds) {
+            selects.push(
+                item[len] + shoppingOffers(price, special, newNeeds)
+            );
+        }
+    }
+    selects.push(
+        needs.reduce((acc, cur, idx) => {
+            return acc + cur * price[idx];
+        }, 0)
+    );
 
-for (let i = 0; i < 10; i++) {
-  (function __prof() {
-    console.log('hello 2')
-  })();
-}
+    return Math.min(...selects);
+};
+const data = [2, 3, 4]
+const data1 = [[1, 1, 0, 4], [2, 2, 1, 9]];
+const data2 = [1, 2, 1];
+console.log(shoppingOffers(data, data1, data2));
---
+++ __test__/test2.js
@@ -0,0 +1,6 @@
+function fun() {
+    let a = 1;
+    return a = 2;
+}
+
+console.log(fun());
